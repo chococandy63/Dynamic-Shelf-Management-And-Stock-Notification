918cca9aff87c206965dc2600adcf703508fb988
Manav Chauhan <manavchan03@gmail.com>
2023-08-18 02:03:52 +0530

    Added few lines of code in CountTrack.jsx for connection to the ThingSpeak API

--- src/components/countTrack/CountTrack.jsx
+++ src/components/countTrack/CountTrack.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import './countTrack.css'
 import Navbar from "../navbar/Navbar";
 const CountTrack = () => {
+  const [availableBoxes, setAvailableBoxes] = useState(1);
+  useEffect(() => {
+    // Function to fetch data from the API and update the state
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.thingspeak.com/channels/2241519/fields/1.json?api_key=GH56CF2YY8IQ2XH1&results=2');
+        const data = await response.json();
+
+        // Assuming the API response structure contains the desired value
+        const latestValue = data.feeds[data.channel.last_entry_id].field1;
+        setAvailableBoxes(latestValue);
+      } catch (error) {
+        console.error('API fetch error:', error);
+      }
+    };
+
+    // Fetch data initially
+    fetchData();
+
+    // Fetch data every 10 seconds (adjust the interval as needed)
+    const intervalId = setInterval(fetchData, 30000);
+
+    // Clean up the interval when the component unmounts
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
   return (
     <div>
       <Navbar></Navbar>
@@ -42,7 +70,7 @@ const CountTrack = () => {
           </svg>
         </div>
         <h5 className="product_name">Dairy Milk</h5>
-          <p className="warehouse_text">Available : 10 boxes </p> 
+          <p className="warehouse_text">Available : {availableBoxes} boxes </p> 
           <button className="order_button">Order</button>
           {/* put variable at the place of 10 boxes */}
         </div>
